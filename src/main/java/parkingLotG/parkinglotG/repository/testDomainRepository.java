70270bf6a39fb2e7ceab2b11f5fa8ef9d3481902
kooyeongmin <32190192@dankook.ac.kr>
2023-10-16 11:07:40 +0900

    중간 전

--- src/main/java/parkingLotG/parkinglotG/repository/testDomainRepository.java
+++ src/main/java/parkingLotG/parkinglotG/repository/testDomainRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import parkingLotG.parkinglotG.domain.TestDomain;
 
-@Repository // 스프링빈에 등록
+@Repository // 스프링빈에 등록 //
 
 public interface testDomainRepository extends JpaRepository<TestDomain,Long> {
 }
